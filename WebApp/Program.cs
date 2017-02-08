3c2aaa40ed8b5274dee4ab3203980faaa78356e4
Menno Morsink <mennomorsink@gmail.com>
2017-02-08 18:59:50 +0100

    added webapp with google oAuth

---
+++ WebApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options => options.UseHttps("Properties/testcert.pfx"))
+                .UseUrls("https://localhost:5001")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
